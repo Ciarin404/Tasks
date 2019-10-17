67525e525ba1038c278ad40fba18bc651de94c59
Ciarin404 <griiffon@gmail.com>
2019-10-17 14:44:02 +0300

    New project was created. Task 1 completed.

---
+++ Module2_1/Module2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            byte m;
+            Console.Write("Введите число компаний:");
+            n = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите % налога:");
+            m = Byte.Parse(Console.ReadLine());
+            int income = 230;
+            float sum;
+            sum = n * income * m / 100; 
+            Console.WriteLine($"Суммарный налог государства:{sum}");
+            Console.ReadKey();
+        }
+    }
+}
