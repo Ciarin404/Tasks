5e995f62719a20018fdf8c6109b32df961a11449
Ciarin404 <griiffon@gmail.com>
2019-10-24 19:00:19 +0300

    Task 4 completed.

---
+++ Module3_4/Module3_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number:");
+            var number = Console.ReadLine().Replace('.', ',');
+            bool isNumber = double.TryParse(number, out double numberNum);
+            char[] numberArray;
+
+            if (isNumber)
+            {
+                if (numberNum >= 0)
+                {
+                    numberArray = number.ToCharArray();
+                    Array.Reverse(numberArray);
+                    number = new string(numberArray);
+                    numberNum = double.Parse(number);
+                    Console.Write($"Reverse number: {numberNum}");
+                }
+                else
+                {
+                    numberNum = -numberNum;
+                    numberArray = number.ToCharArray();
+                    Array.Reverse(numberArray);
+                    number = new string(numberArray);
+                    numberNum = double.Parse(number);
+                    Console.Write($"Reverse number: {-numberNum}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered string is not a number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
